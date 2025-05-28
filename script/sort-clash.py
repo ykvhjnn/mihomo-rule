55ee83073ec3f759f4261c843c1a65658bda328d
ykvhjnn <y13796820528@163.com>
2025-05-28 19:02:25 +0800

    Update sort-clash.py

--- script/sort-clash.py
+++ script/sort-clash.py
@@ -47,8 +47,9 @@ def extract_domain(line: str) -> str | None:
         return None
     for prefix, offset in [
         ("DOMAIN,", 7),
-        ("DOMAIN-SUFFIX,", 14),
+        ("DOMAINSUFFIX,", 14),
         ("+.", 2),
+        ("*.", 2),
         (".", 1)
     ]:
         if line.startswith(prefix):
